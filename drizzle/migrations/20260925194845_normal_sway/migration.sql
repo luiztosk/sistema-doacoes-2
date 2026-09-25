138334ce63dce2058b1296ea076ebec5bdc8aa93
Luiz Tosk <6207512+luiztosk@users.noreply.github.com>
2026-09-25 16:58:39 -0300

    fix seed to work with new schema

--- drizzle/migrations/20260925194845_normal_sway/migration.sql
+++ drizzle/migrations/20260925194845_normal_sway/migration.sql
@@ -1,6 +1,5 @@
 CREATE TABLE `assistido` (
 	`id` text PRIMARY KEY,
-	`organization_id` text NOT NULL,
 	`nome` text NOT NULL,
 	`telefone` text,
 	`email` text,
@@ -25,29 +24,23 @@ CREATE TABLE `assistido` (
 	`atividade_remunerada` integer,
 	`renda` real,
 	`crianca_escola` integer,
-	`observacoes` text,
-	CONSTRAINT `fk_assistido_organization_id_organization_id_fk` FOREIGN KEY (`organization_id`) REFERENCES `organization`(`id`)
+	`observacoes` text
 );
 --> statement-breakpoint
 CREATE TABLE `categoria_item` (
 	`id` text PRIMARY KEY,
-	`organization_id` text NOT NULL,
-	`nome` text NOT NULL,
-	CONSTRAINT `fk_categoria_item_organization_id_organization_id_fk` FOREIGN KEY (`organization_id`) REFERENCES `organization`(`id`)
+	`nome` text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `coleta` (
 	`id` text PRIMARY KEY,
-	`organization_id` text NOT NULL,
 	`doador_id` text,
 	`data_hora` integer,
-	CONSTRAINT `fk_coleta_organization_id_organization_id_fk` FOREIGN KEY (`organization_id`) REFERENCES `organization`(`id`),
 	CONSTRAINT `fk_coleta_doador_id_doador_id_fk` FOREIGN KEY (`doador_id`) REFERENCES `doador`(`id`)
 );
 --> statement-breakpoint
 CREATE TABLE `doador` (
 	`id` text PRIMARY KEY,
-	`organization_id` text NOT NULL,
 	`nome` text NOT NULL,
 	`telefone` text,
 	`email` text,
@@ -57,29 +50,24 @@ CREATE TABLE `doador` (
 	`complemento` text,
 	`bairro` text,
 	`cidade` text,
-	`uf` text(2),
-	CONSTRAINT `fk_doador_organization_id_organization_id_fk` FOREIGN KEY (`organization_id`) REFERENCES `organization`(`id`)
+	`uf` text(2)
 );
 --> statement-breakpoint
 CREATE TABLE `entrega` (
 	`id` text PRIMARY KEY,
-	`organization_id` text NOT NULL,
 	`assistido_id` text,
 	`data_hora` integer,
-	CONSTRAINT `fk_entrega_organization_id_organization_id_fk` FOREIGN KEY (`organization_id`) REFERENCES `organization`(`id`),
 	CONSTRAINT `fk_entrega_assistido_id_assistido_id_fk` FOREIGN KEY (`assistido_id`) REFERENCES `assistido`(`id`)
 );
 --> statement-breakpoint
 CREATE TABLE `item` (
 	`id` text PRIMARY KEY,
-	`organization_id` text NOT NULL,
 	`nome_id` text NOT NULL,
 	`status` text DEFAULT 'AGUARDA_COLETA' NOT NULL,
 	`coleta_id` text,
 	`entrega_id` text,
 	`doador_id` text,
 	`assistido_id` text,
-	CONSTRAINT `fk_item_organization_id_organization_id_fk` FOREIGN KEY (`organization_id`) REFERENCES `organization`(`id`),
 	CONSTRAINT `fk_item_nome_id_nome_item_id_fk` FOREIGN KEY (`nome_id`) REFERENCES `nome_item`(`id`),
 	CONSTRAINT `fk_item_coleta_id_coleta_id_fk` FOREIGN KEY (`coleta_id`) REFERENCES `coleta`(`id`),
 	CONSTRAINT `fk_item_entrega_id_entrega_id_fk` FOREIGN KEY (`entrega_id`) REFERENCES `entrega`(`id`),
@@ -89,10 +77,8 @@ CREATE TABLE `item` (
 --> statement-breakpoint
 CREATE TABLE `nome_item` (
 	`id` text PRIMARY KEY,
-	`organization_id` text NOT NULL,
 	`categoria_id` text NOT NULL,
 	`nome` text NOT NULL,
-	CONSTRAINT `fk_nome_item_organization_id_organization_id_fk` FOREIGN KEY (`organization_id`) REFERENCES `organization`(`id`),
 	CONSTRAINT `fk_nome_item_categoria_id_categoria_item_id_fk` FOREIGN KEY (`categoria_id`) REFERENCES `categoria_item`(`id`)
 );
 --> statement-breakpoint
